473be87bdb692aca8f4dbee0d4627ebf96814573
Antonyfrtz <antonyfrtz@gmail.com>
2023-02-05 23:28:28 +0200

    Created Pool Screen, Added Functions
    - Pool Assets
    - Pool Code

--- WpfApp1/UserHome1.xaml.cs
+++ WpfApp1/UserHome1.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -96,5 +97,6 @@ namespace WpfApp1
             backToLogin.Show();
             this.Close();
         }
+
     }
 }
--- WpfApp1/UserMenuItems/UserControlPool.xaml.cs
+++ WpfApp1/UserMenuItems/UserControlPool.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,90 @@ namespace WpfApp1.UserMenuItems
         public UserControlPool()
         {
             InitializeComponent();
+        }
+
+        bool LightBulbisChecked = false;
+        bool AlarmisChecked = false;
+
+        private void ToggleButtons_Click(object sender, RoutedEventArgs e) ///This changes the icons next to the toggle button for alarm and lights.
+        {
+            string txtbx = ((ToggleButton)sender).Name;
+            switch (txtbx)
+            {
+                case "LightsBtn": ///this is for lightbulb
+                    if (LightBulbisChecked)
+                    {
+                        Lightbulb.Kind = (MaterialDesignThemes.Wpf.PackIconKind)Enum.Parse(typeof(MaterialDesignThemes.Wpf.PackIconKind), "LightbulbOnOutline");
+                        poolIcon.Source = new BitmapImage(new Uri("/Assets/outdoor_pool_w_light.jpg", UriKind.Relative));
+                    }
+                    else
+                    {
+                        Lightbulb.Kind = (MaterialDesignThemes.Wpf.PackIconKind)Enum.Parse(typeof(MaterialDesignThemes.Wpf.PackIconKind), "LightbulbOffOutline");
+                        poolIcon.Source = new BitmapImage(new Uri("/Assets/outdoor_pool_nolight.jpg", UriKind.Relative));
+                    }
+                    LightBulbisChecked = !LightBulbisChecked;
+                    break;
+
+                case "AlarmBtn": ///this is for Alarm
+                    if (AlarmisChecked)
+                    {
+                        Alarm.Kind = (MaterialDesignThemes.Wpf.PackIconKind)Enum.Parse(typeof(MaterialDesignThemes.Wpf.PackIconKind), "AlarmLight");
+                    }
+                    else
+                    {
+                        Alarm.Kind = (MaterialDesignThemes.Wpf.PackIconKind)Enum.Parse(typeof(MaterialDesignThemes.Wpf.PackIconKind), "AlarmLightOff");
+                    }
+                    AlarmisChecked = !AlarmisChecked;
+                    break;
+            }
+        }
+
+        private void Slider_Changed(object sender, RoutedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            if (slider != null)
+            {
+                if (slider.Value > 18)
+                {
+                    slider.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#E1341E");
+                    if (tempLabel != null)
+                    {
+                        tempLabel.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#E1341E");
+                    }
+                }
+                else
+                {
+                    slider.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#1a78c2");
+                    if (tempLabel != null)
+                    {
+                        tempLabel.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#1a78c2");
+                    }
+                }
+            }
+        }
+
+        private void Slider_PoolLevel(object sender, RoutedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            if (Level != null)
+            {
+                if (slider.Value == 1)
+                {
+
+                    Level.Text = "Half-Filled";
+                    slider.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#7fbded");
+                }
+                else if (slider.Value == 2)
+                {
+                    Level.Text = "Filled";
+                    slider.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#1a78c2");
+                }
+                else
+                {
+                    Level.Text = "Empty";
+                    slider.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("Gray");
+                }
+            }
         }
     }
 }
