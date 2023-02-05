f87d527486d251a80dcaa397815b07fcfcd0cf14
Antonyfrtz <antonyfrtz@gmail.com>
2023-02-05 18:41:19 +0200

    Finished File Indexing, Added Controls, Visual Overhaul
    - Used folders for Assets and User Controls
    - Improved functionality of menu

--- WpfApp1/UserHome1.xaml.cs
+++ WpfApp1/UserHome1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BespokeFusion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApp1.UserMenuItems;
 
 namespace WpfApp1
 {
@@ -21,7 +23,10 @@ namespace WpfApp1
     {
         public UserHome1()
         {
-            InitializeComponent();
+            InitializeComponent(); // by defaults opens to homepage
+            UserControl main = new UserControlHome();
+            ListViewMenu.SelectedItem = ItemHome;
+            GridMain.Children.Add(main);
         }
 
         private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
@@ -38,22 +43,58 @@ namespace WpfApp1
 
         private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            UserControl usc = null;
-            GridMain.Children.Clear();
-
+            GridMain.Children.Clear(); // remove current selection from main grid
             switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
             {
                 case "ItemHome":
-                    usc = new UserControlHome();
-                    GridMain.Children.Add(usc);
+                    GridMain.Children.Add(new UserControlHome());
                     break;
-                case "ItemCreate":
-                    usc = new UserControlCreate();
-                    GridMain.Children.Add(usc);
+                case "ItemPool":
+                    GridMain.Children.Add(new UserControlPool());
+                    break;
+                case "ItemTrojanHorse":
+                    GridMain.Children.Add(new UserControlTrojanHorse());
+                    break;
+                case "ItemRestaurant":
+                    GridMain.Children.Add(new UserControlRestaurant());
                     break;
                 default:
                     break;
             }
+            if (ButtonCloseMenu.Visibility == Visibility.Visible)
+            {
+                ButtonCloseMenu.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Primitives.ButtonBase.ClickEvent));
+            }
+        }
+
+        private void InfoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CustomMaterialMessageBox msg = new CustomMaterialMessageBox
+            {
+                TxtMessage = { Text = "In the left part of the screen there is a menu which includes all the utilities you have access to. \n\n" +
+                "To enter the pool managment system click the pool icon from the menu. \n" +
+                "To use the Trojan Horse RV you rented click the trojan horse icon from the menu. \n" +
+                "If you want to enter the restaurant and order food or drinks click the restaurant icon from the menu. \n" +
+                "If you want to return to the home screen click the home icon from the menu. \n\n" +
+                "In the right top part of the screen there is a three dot button. If you click it you can logout and you can also see your account's details. \n", Foreground = Brushes.Black },
+                TxtTitle = { Text = "Information", Foreground = Brushes.White },
+                BtnOk = { Content = "Okay", Background = Brushes.DarkSlateGray },
+                BtnCancel = { Content = "Cancel", Visibility = Visibility.Collapsed },
+                MainContentControl = { Background = Brushes.Bisque },
+                TitleBackgroundPanel = { Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#2e3546") },
+
+                BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#2e3546")
+            };
+
+            msg.Show();
+            MessageBoxResult results = msg.Result;
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow backToLogin = new MainWindow();
+            backToLogin.Show();
+            this.Close();
         }
     }
 }
