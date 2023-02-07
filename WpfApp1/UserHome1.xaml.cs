fe05741c9383611fdb5148f9dba51f6700c953bf
Antonyfrtz <antonyfrtz@gmail.com>
2023-02-07 23:35:06 +0200

    Major overhaul of Trojan Horse interface
    -Moved around visuals
    -Working navigation and switching
    -Working movement
    
    TBD
    -Mapview using latest coords

--- WpfApp1/UserHome1.xaml.cs
+++ WpfApp1/UserHome1.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using BespokeFusion;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Web.WebView2.Core;
 using System;
 using System.IO;
 using System.Windows;
@@ -91,22 +93,49 @@ namespace WpfApp1
 
         private void MoveUp_Click(object sender, RoutedEventArgs e)
         {
-            
+            lat +=0.000080;
+            lng +=0.000050;
+            origin = "new google.maps.LatLng(" + lat.ToString() + ", " + lng.ToString() + ")";
+            webBrowser1.ExecuteScriptAsync("changePos("+ origin +");"); // else just change the destination with calcroute
+            if(webBrowser1.Source == new Uri(AppDomain.CurrentDomain.BaseDirectory + "html\\map_route.html")) { // if we are navigating, refresh navigation
+                webBrowser1.ExecuteScriptAsync("calcRoute(" + origin + "," + dest + ");");
+            }
         }
 
         private void MoveDown_Click(object sender, RoutedEventArgs e)
         {
-            
+            lat -= 0.000080;
+            lng -= 0.000050;
+            origin = "new google.maps.LatLng(" + lat.ToString() + ", " + lng.ToString() + ")";
+            webBrowser1.ExecuteScriptAsync("changePos(" + origin + ");"); // else just change the destination with calcroute
+            if (webBrowser1.Source == new Uri(AppDomain.CurrentDomain.BaseDirectory + "html\\map_route.html"))
+            { // if we are navigating, refresh navigation
+                webBrowser1.ExecuteScriptAsync("calcRoute(" + origin + "," + dest + ");");
+            }
         }
 
         private void MoveLeft_Click(object sender, RoutedEventArgs e)
         {
-            
+            lat += 0.000030;
+            lng -= 0.000150;
+            origin = "new google.maps.LatLng(" + lat.ToString() + ", " + lng.ToString() + ")";
+            webBrowser1.ExecuteScriptAsync("changePos(" + origin + ");"); // else just change the destination with calcroute
+            if (webBrowser1.Source == new Uri(AppDomain.CurrentDomain.BaseDirectory + "html\\map_route.html"))
+            { // if we are navigating, refresh navigation
+                webBrowser1.ExecuteScriptAsync("calcRoute(" + origin + "," + dest + ");");
+            }
         }
 
         private void MoveRight_Click(object sender, RoutedEventArgs e)
         {
-            
+            lat -= 0.000030;
+            lng += 0.000150;
+            origin = "new google.maps.LatLng(" + lat.ToString() + ", " + lng.ToString() + ")";
+            webBrowser1.ExecuteScriptAsync("changePos(" + origin + ");"); // else just change the destination with calcroute
+            if (webBrowser1.Source == new Uri(AppDomain.CurrentDomain.BaseDirectory + "html\\map_route.html"))
+            { // if we are navigating, refresh navigation
+                webBrowser1.ExecuteScriptAsync("calcRoute(" + origin + "," + dest + ");");
+            }
         }
 
         private void Park_Click(object sender, RoutedEventArgs e)
@@ -114,49 +143,76 @@ namespace WpfApp1
             popup.IsOpen = true;
         }
 
-        private void Navigation_Click(object sender, RoutedEventArgs e)//For Gardens
+        // Coordinates - declared as double so they can be changed when the trojan horse is moved
+        double lat=35.667595;
+        double lng=139.776457;
+        // String coordinates to be used for javascript calls
+        private string origin = "";
+        private string dest = "";
+        private void Gardens_Click(object sender, RoutedEventArgs e)//For Gardens
         {
-            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "html\\map_route.html"))
+            // format strings for js call
+            origin = "new google.maps.LatLng(" + lat.ToString() + ", " + lng.ToString() + ")";
+            dest = "new google.maps.LatLng(35.666156, 139.776646)";
+            if (webBrowser1.Source == new Uri(AppDomain.CurrentDomain.BaseDirectory + "html\\mapview.html"))
             {
-                StreamReader objReader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "html\\map_route.html");
-                string line = "";
-                line = objReader.ReadToEnd();
-                objReader.Close();
-                line = line.Replace("[origin]", "35.667595, 139.776457");
-                line = line.Replace("[destination]", "35.666156, 139.776646");
-                StreamWriter page = File.CreateText(AppDomain.CurrentDomain.BaseDirectory + "html\\created_map.html");
-                page.Write(line);
-                page.Close();
-                Uri uri = new Uri(AppDomain.CurrentDomain.BaseDirectory + "html\\created_map.html");
-                webBrowser1.Source = uri;
+                InitializeAsync(); // if no route has been loaded yet, wait for route map to load in
+            }
+            else
+            {
+                webBrowser1.ExecuteScriptAsync("calcRoute(" + origin + "," + dest + ");"); // else just change the destination with calcroute
+            }
+        }
+        private void PalaceOfZeus_Click(object sender, RoutedEventArgs e)//For Palace of Zeus
+        {
+            origin = "new google.maps.LatLng(" + lat.ToString() + ", " + lng.ToString() + ")";
+            dest = "new google.maps.LatLng(35.667262, 139.777619)";
+            if (webBrowser1.Source == new Uri(AppDomain.CurrentDomain.BaseDirectory + "html\\mapview.html"))
+            {
+                InitializeAsync();
+            }
+            else
+            {
+                webBrowser1.ExecuteScriptAsync("calcRoute(" + origin + "," + dest + ");");
             }
         }
 
-        private void Navigation_Click1(object sender, RoutedEventArgs e)//For Palace of Zeus
+        // we need to wait for map initialization (page load) before running our calscript
+        private async void InitializeAsync() //starts loading content asynchronously
         {
-
+            await webBrowser1.EnsureCoreWebView2Async(null);
+            webBrowser1.CoreWebView2.DOMContentLoaded += OnWebViewDOMContentLoaded;
+            Uri uri = new Uri(AppDomain.CurrentDomain.BaseDirectory + "html\\map_route.html");
+            webBrowser1.Source = uri;
         }
+        private async void OnWebViewDOMContentLoaded(object sender, CoreWebView2DOMContentLoadedEventArgs arg)
+        {
+            webBrowser1.CoreWebView2.DOMContentLoaded -= OnWebViewDOMContentLoaded;
+
+            webBrowser1.Focus();
+
+            // now we can load destination
+            await webBrowser1.ExecuteScriptAsync("calcRoute("+origin+","+dest+");");
+        }
+
         private void OpenDoor_Click(object sender, RoutedEventArgs e)
         {
-            //horse_maker.icon:{
-            //  url:'',
-            //scaledSize: new google.maps.Size(32, 32)
-            //}
+            webBrowser1.ExecuteScriptAsync("parkPos('https://lh3.googleusercontent.com/H_iRj67z5zTIKKINEIDgWQ8-ZBYC2Bx3ls_9VbY0nI6MNevv75DfBxNaeHBX2l6sjwW03BwU8ONkb7PFeu3gvCzVocVuF6jAaVY0fcwy')");
         }
 
         private void HalfOpenDoor_Click(object sender, RoutedEventArgs e)
         {
-
+            webBrowser1.ExecuteScriptAsync("parkPos('https://lh3.googleusercontent.com/enwRMlXNFnSYKvMZQsIjEBoSp9uHPXJ06duqN-3iSEBiRhszuYWCSE7emEjZM1F0vQQucC9b_PUYTfWfDcqIaNyyn2jgXQp8oXfF7IYU')");
         }
 
         private void CloseDoor_Click(object sender, RoutedEventArgs e)
         {
-
+            webBrowser1.ExecuteScriptAsync("parkPos('https://lh3.googleusercontent.com/Dvym3JTcDxohGT_UeDCMoyWxObOguT5l1DVvHf1rvRfoDuzXn_1AteNK9ZFJiLbcCGDJChsR98UKUp94D7A0_oChkqdtkEu44TyUIpKb')");
         }
 
         private void Ladder_Click(object sender, RoutedEventArgs e)
         {
-
+            webBrowser1.ExecuteScriptAsync("parkPos('https://lh3.googleusercontent.com/n_s7CzvdlevFxOL7t7YfzvddL-uAHq2xf_Xu3Rapdw0hiyGRqP8rYFH10bVbcpiMI6QAYxo6TYz_xOtdPZZIbHIdQKwM1LtstEkEf_s')"); 
         }
 
         String sURL = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\html\\mapview.html";
@@ -165,5 +221,13 @@ namespace WpfApp1
             Uri uri = new Uri(sURL);
             webBrowser1.Source = uri;
         }
-}
+
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri(sURL);
+            webBrowser1.Source = uri;
+            webBrowser1.ExecuteScriptAsync("changePos(" + origin + ");");
+        }
+    }
 }
